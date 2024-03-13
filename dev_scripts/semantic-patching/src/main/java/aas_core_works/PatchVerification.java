7af7a84fa0fbc74810186606c28af26c076b0b8a
mboehm <markus.boehm@conplement.de>
2024-03-13 14:37:05 +0100

    Remove unused import

--- dev_scripts/semantic-patching/src/main/java/aas_core_works/PatchVerification.java
+++ dev_scripts/semantic-patching/src/main/java/aas_core_works/PatchVerification.java
@@ -11,7 +11,6 @@ import java.io.FileWriter;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
-import java.util.List;
 import java.util.Optional;
 
 public class PatchVerification {
