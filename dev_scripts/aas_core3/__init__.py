8be845e0099ba1f02fa5d5de369bf3403f82d02b
Tobias Langer <tobias.langer@conplement.de>
2024-07-23 13:25:50 +0200

    Check explicitly for invalid model types
    We explicitly check during the JSON de-serialization that model types
    correspond to the expected model types. We need to be particularly
    careful with concrete classes without descendants with a mandatory
    modelType, as they do not necessarily require a model type for
    de-serialization, but the specs mandate it for reasons of backward
    compatibility.
    
    The code corresponds to aas-core-codegen 41badc20, and the test data
    corresponds to aas-core3.0-testgen 9e523511c.
    
    This is related to the issue aas-core3.0-python #32, which discovered
    the problematic in the first place.

--- dev_scripts/aas_core3/__init__.py
+++ dev_scripts/aas_core3/__init__.py
@@ -3,5 +3,5 @@ Provide Python SDK as copied from aas-core-codegen test data.
 
 This copy is necessary so that we can decouple from ``aas-core*-python`` repository.
 
-The revision of aas-core-codegen was: 5a9ef8c6
+The revision of aas-core-codegen was: bd310fc2
 """
